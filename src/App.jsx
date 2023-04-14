d4161b39058885f593e3313e4d3e3e440b0fbcd1
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-04-14 18:07:53 -0500

    Add Routes and general styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import AnimeGenres from './components/animeGenres';
+import GenreDetails from './components/genreDetails';
+import Header from './components/header';
+
 function App() {
+  const { genreId } = useSelector((state) => state.genreDetails);
+
   return (
     <div className="App">
-      <h1>Title</h1>
+      <BrowserRouter>
+
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<AnimeGenres />} />
+          <Route path="/details" element={<GenreDetails genreId={genreId} />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
